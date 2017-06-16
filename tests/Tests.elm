c8f11b6543c0ef6c6d6c725cc9cd856e421b0594
Fabien HENON <henon.fabien@softcreations.fr>
2017-06-16 09:16:09 +0200

    Add isLoading function

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -1,12 +1,8 @@
 module Tests exposing (..)
 
--- import Expect
-
+import Expect
 import Test exposing (..)
 import InfiniteScroll as IS
-
-
--- import Time exposing (second)
 
 
 loadMore : IS.Direction -> Cmd msg
@@ -22,26 +18,22 @@ init =
 all : Test
 all =
     describe "elm-infinite-scroll"
-        []
-
-
-
---  describe "startLoading"
---     [ test "set isLoading to True" <|
---         \() ->
---             let
---                 model =
---                     { init | isLoading = False }
---             in
---                 Expect.equal (IS.startLoading model).isLoading True
---     ]
--- , describe "stopLoading"
---     [ test "set isLoading to False" <|
---         \() ->
---             let
---                 model =
---                     { init | isLoading = True }
---             in
---                 Expect.equal (IS.stopLoading model).isLoading False
---     ]
--- ]
+        [ describe "startLoading"
+            [ test "set isLoading to True" <|
+                \() ->
+                    let
+                        model =
+                            init |> IS.stopLoading
+                    in
+                        Expect.equal (IS.startLoading model |> IS.isLoading) True
+            ]
+        , describe "stopLoading"
+            [ test "set isLoading to False" <|
+                \() ->
+                    let
+                        model =
+                            init |> IS.startLoading
+                    in
+                        Expect.equal (IS.stopLoading model |> IS.isLoading) False
+            ]
+        ]
